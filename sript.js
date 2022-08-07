bb65d7f1812e852008fe9cc68179bc4f69ad0bb9
january-whale <110626769+january-whale@users.noreply.github.com>
2022-08-07 14:38:06 +0300

    Добавила страницу "Обо мне"

---
+++ sript.js
@@ -0,0 +1,6 @@
+alert('Привет! Нажми на кнопку Тык сюда');
+const helloButton = document.getElementById('hello-btn');
+
+helloButton.addEventListener('click', () => {
+    alert('Улыбнись! Желаю хорошего дня и прекрасного настроения! Если первый мой первый call-to-action не сработал, то повторяю: Улыбнись! Все, теперь точно сработало))');
+})
